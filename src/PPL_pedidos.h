0efff8506b55327dad4b5f877f65272d111defa7
lincomanGuillermo <holasoyguishe@gmail.com>
2021-10-12 15:04:32 -0300

    pedidos

--- src/PPL_pedidos.h
+++ src/PPL_pedidos.h
@@ -5,8 +5,24 @@
  *      Author: Compumar
  */
 #include "PPL_inputs.h"
+
+struct
+{
+ int idEmpresa;
+ int idPedido;
+ int kilosHDPE;
+ int kilosLPDE;
+ int kilosPP;
+ int kilosTotales;
+ int estado;
+ int isEmpty;
+}typedef ePedido;
+
 #ifndef PPL_PEDIDOS_H_
 #define PPL_PEDIDOS_H_
+int initClient(eClient list[], int len);
+int BuscarLibre(eClient list[], int len);
+eClient addClientNew(int idNuevoCliente);
 
 
 
